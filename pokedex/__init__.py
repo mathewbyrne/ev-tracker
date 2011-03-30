4fd201f108362a777f9d762235da091897820763
Mathew Byrne <mathewbyrne@gmail.com>
2011-03-30 18:21:30 +1100

    Refactored internals of pokedex.

--- pokedex/__init__.py
+++ pokedex/__init__.py
@@ -6,7 +6,7 @@ import os
 import sqlite3
 import difflib
 
-from .pokemon import Species, EvSet
+from pokemon import Species, EvSet
 
 
 __all__ = ['NoSuchSpecies', 'AmbiguousSpecies', 'fetch', 'search']
@@ -17,28 +17,34 @@ _connection = sqlite3.connect(_DB_FILE)
 _connection.row_factory = sqlite3.Row
 
 
-_id_cache = {}
-_name_cache = {}
+class _SpeciesCache(object):
+	'''
+	A very simple caching mechanism for database species records. There are
+	kept in memory until requested again.
+	'''
+	def __init__(self):
+		self._cache = {'id': {}, 'name': {}}
+	
+	def search(key, value):
+		return self._cache[key][value]
+	
+	def add(species):
+		self._cache['id'][species.id] = species
+		self._cache['name'][species.name] = species
 
 
-def _cache(species):
-    _id_cache[species.id] = species
-    _name_cache[species.name] = species
-
-
-def _get_names():
-    return [row[0] for row in _connection.execute('''SELECT name FROM pokemon''')]
+_cache = _SpeciesCache()
 
 
 class NoSuchSpecies(Exception):
-    """Raised when a search for a Pokemon species fails."""
+    '''Raised when a search for a Pokemon species fails.'''
     def __init__(self, identifier):
         super(NoSuchSpecies, self).__init__()
         self.identifier = identifier
 
 
 class AmbiguousSpecies(NoSuchSpecies):
-    """Raised when several matches are found for a Pokemon name search."""
+    '''Raised when several matches are found for a Pokemon name search.'''
     def __init__(self, identifier, matches):
         super(AmbiguousSpecies, self).__init__(identifier)
         self.matches = matches
@@ -47,7 +53,28 @@ class AmbiguousSpecies(NoSuchSpecies):
 _SELECT_SQL = '''SELECT p.id AS id, name, ev_hp, ev_attack, ev_defense,
                  ev_special_attack, ev_special_defense, ev_speed
                  FROM pokemon AS p
-                 JOIN stats AS s ON p.id = s.pokemon_id'''
+                 JOIN stats AS s ON p.id = s.pokemon_id
+                 WHERE %s = ?'''
+
+
+def _fetch(field, value):
+    assert(field in ['id', 'name'])
+    
+    try:
+        return _cache.search(field, value)
+	except KeyError:
+		pass
+    
+    rows = _connection.execute(SELECT_SQL % field, (value,)).fetchall()
+    
+    if len(rows) == 0:
+        raise NoSuchSpecies(value)
+    
+    return rows
+
+
+def _name_list():
+    return [row[0] for row in _connection.execute('''SELECT name FROM pokemon''')]
 
 
 def fetch(id=None, name=None):
@@ -58,19 +85,8 @@ def fetch(id=None, name=None):
     '''
     assert(id is not None or name is not None)
     
-    if id is not None:
-        if id in _id_cache:
-            return _id_cache[id]
-        rows = _connection.execute('%s %s' % (_SELECT_SQL, 'WHERE p.id = ?'),
-                                   (id,)).fetchall()
-        row = rows[0]
-    
-    elif name is not None:
-        if name in _name_cache:
-            return _name_cache[name]
-        rows = _connection.execute('%s %s' % (_SELECT_SQL, 'WHERE p.name = ?'),
-                                   (name,)).fetchall()
-        row = rows[0]
+    rows = _fetch('id', id) if name is None else _fetch('name', name)
+    row = rows[0]
     
     evs = EvSet({'HP': row['ev_hp'],
                  'Attack': row['ev_attack'],
@@ -80,7 +96,7 @@ def fetch(id=None, name=None):
                  'Speed': row['ev_speed']})
     species = Species(id=row['id'], name=row['name'], evs=evs)
     
-    _cache(species)
+    _cache.add(species)
     
     return species
 
@@ -91,19 +107,19 @@ def search(input):
     or integer value corresponding to the name or number of a species. If the
     string is a name, the search will attempt to find close matches as well
     as an exact match.
+    
+    Will raise NoSuchSpecies if no match is found, or AmbiguousSpecies if 
+    there are close matches, but nothing exact.
     '''
     if input.isdigit():
-        try:
-            return fetch(id=int(input))
-        except KeyError:
-            raise NoSuchSpecies(input)
+        return fetch(id=int(input))
     else:
         try:
             return fetch(name=input.lower())
-        except KeyError:
-            matches = difflib.get_close_matches(input, _get_names(), n=3)
+        except NoSuchSpecies as e:
+            matches = difflib.get_close_matches(input, _name_list(), n=3)
             if len(matches) == 0:
-                raise NoSuchSpecies(input)
+                raise e
             else:
                 raise AmbiguousSpecies(input, matches)
 
