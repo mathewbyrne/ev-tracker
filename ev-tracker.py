61c688229b10330fb4128e3ebfe98ed9f01777d1
Mathew Byrne <mathewbyrne@gmail.com>
2012-10-16 11:02:26 +1100

    Fixed some minor bugs.

--- ev-tracker.py
+++ ev-tracker.py
@@ -16,7 +16,7 @@ TRACKER_PATH = os.path.expanduser(os.path.join('~', TRACKER_FILENAME))
 
 
 class Tracker(object):
-    
+
     @classmethod
     def from_json(cls, filename):
         tracker = cls()
@@ -31,9 +31,9 @@ class Tracker(object):
                     tracker.active = pokemon
         except IOError:
             pass  # Ignore missing tracking file.
-        
+
         return tracker
-    
+
     @staticmethod
     def to_json(tracker, filename=None):
         filename = tracker.filename if filename is None else filename
@@ -42,49 +42,49 @@ class Tracker(object):
         if tracker.has_active():
             data['active'] = tracker.active.id
         data['pokemon'] = [pokemon.to_dict() for pokemon in tracker.pokemon.values()]
-        
+
         json.dump(data, fp)
         fp.close()
-    
+
     pokemon = {}
-    
+
     def __init__(self):
         self._active = None
         self.counter = 1
-    
+
     active = property(lambda self: self.get_active(),
                       lambda self, pokemon: self.set_active(pokemon))
-    
+
     def has_active(self):
         return self._active is not None
-    
+
     def get_active(self):
         if self._active is None:
             raise NoActivePokemon()
         return self._active
-    
+
     def set_active(self, pokemon):
         self._active = pokemon
-    
+
     def get_pokemon(self, id):
         if id not in self.pokemon:
             raise NoTrackedPokemon(id)
         return self.pokemon[id]
-    
+
     def unique_id(self):
         while self.counter in self.pokemon:
             self.counter += 1
         return self.counter
-    
+
     def track(self, pokemon):
         self.pokemon[pokemon.id] = pokemon
-    
+
     def untrack(self, pokemon):
         del self.pokemon[pokemon.id]
         pokemon.id = None
         if self._active is pokemon:
             self._active = None
-    
+
     def __str__(self):
         if len(self.pokemon):
             return '\n'.join([pokemon.listing(self._active) for pokemon in self.pokemon.values()])
@@ -161,9 +161,9 @@ def _cmd_battle(args):
         pokemon = _tracker.active
     else:
         pokemon = _tracker.get_pokemon(args.id)
-    
+
     pokemon.battle(species)
-    
+
     print evs
     print pokemon
 
@@ -190,43 +190,43 @@ def _build_parser():
                              information to. This defaults to %s in your
                              home directory
                              ''' % TRACKER_FILENAME)
-    
+
     subparsers = parser.add_subparsers()
-    
+
     ev_parser = subparsers.add_parser('ev', help='List Effort Values for a Pokemon')
     ev_parser.add_argument('species', help='Name or number of Pokemon species to search for')
     ev_parser.set_defaults(func=_cmd_ev)
-    
+
     list_parser = subparsers.add_parser('list', help='List tracked Pokemon')
     list_parser.set_defaults(func=_cmd_list)
-    
+
     track_parser = subparsers.add_parser('track', help='Add a Pokemon to track')
     track_parser.add_argument('species', help='Name of number of Pokemon species to track')
     track_parser.add_argument('--name', '-n', help='Nickname of Pokemon')
     track_parser.add_argument('--pokerus', '-p', action='store_true', default=False, help='Indicates the given Pokemon has Pokerus')
     track_parser.add_argument('--item', '-i')
     track_parser.set_defaults(func=_cmd_track)
-    
+
     active_parser = subparsers.add_parser('active', help='List the active Pokemon')
     active_parser.add_argument('--switch', '-s', type=int, help='Switch the active Pokemon')
     active_parser.set_defaults(func=_cmd_active)
-    
+
     status_parser = subparsers.add_parser('status', help='Show the status of the active Pokemon')
     status_parser.add_argument('--id', '-i', type=int)
     status_parser.set_defaults(func=_cmd_status)
-    
+
     update_parser = subparsers.add_parser('update', help='Update a tracked Pokemon\'s details')
     update_parser.set_defaults(func=_cmd_update)
-    
+
     battle_parser = subparsers.add_parser('battle', help='Record a battle for a tracked Pokemon')
-    track_parser.add_argument('species', help='Name of number of Pokemon species to battle')
-    status_parser.add_argument('--id', '-i', type=int)
+    battle_parser.add_argument('species', help='Name of number of Pokemon species to battle')
+    battle_parser.add_argument('--id', '-i', type=int)
     battle_parser.set_defaults(func=_cmd_battle)
-    
+
     release_parser = subparsers.add_parser('release', help='Stop tracking a Pokemon')
     release_parser.add_argument('id', type=int)
     release_parser.set_defaults(func=_cmd_release)
-    
+
     return parser
 
 
--- pokemon.py
+++ pokemon.py
@@ -12,17 +12,17 @@ ITEMS = {
 
 
 class EvSet(object):
-    
+
     STATS = ['hp', 'attack', 'defense', 'special_attack', 'special_defense', 'speed']
     LABELS = ['HP', 'Attack', 'Defense', 'Special Attack', 'Special Defense', 'Speed']
-    
+
     MAX_STAT = 255
     MAX_EV = 510
-    
+
     @staticmethod
     def label(stat):
         return EvSet.LABELS[EvSet.STATS.index(stat)]
-    
+
     def __init__(self, hp=0, attack=0, defense=0, special_attack=0,
                  special_defense=0, speed=0):
         self.hp = int(hp)
@@ -31,62 +31,62 @@ class EvSet(object):
         self.special_attack = int(special_attack)
         self.special_defense = int(special_defense)
         self.speed = int(speed)
-    
+
     def __iadd__(self, other):
         for stat in EvSet.STATS:
             self.__dict__[stat] += other.__dict__[stat]
         return self
-    
+
     def __add__(self, other):
         evs = self.clone()
         evs += other
         return evs
-    
+
     def __imul__(self, integer):
         for stat in EvSet.STATS:
             self.__dict__[stat] *= integer
         return self
-    
+
     def __mul__(self, integer):
         evs = self.clone()
         for stat in EvSet.STATS:
             evs.__dict__[stat] *= integer
         return evs
-    
+
     def __str__(self):
         ev_string = ['+%d %s' % (ev, EvSet.label(stat)) for stat, ev in self.to_dict().items() if ev > 0]
         return ', '.join(ev_string)
-    
+
     def verbose(self):
         ev_string = ['%s: %d' % (EvSet.label(stat), ev) for stat, ev in self.evs.items()]
         if not len(ev_string):
             return 'No EVs'
         return '\n'.join(ev_string)
-    
+
     def clone(self):
         return EvSet(**self.to_dict())
-    
+
     def to_dict(self):
         dict = {}
         for stat in EvSet.STATS:
             dict[stat] = self.__dict__[stat]
         return dict
-            
+
 
 
 class Species(object):
-    
+
     def __init__(self, id, name, evs=None):
         self.id = int(id)
         self.name = name
         self.evs = EvSet() if evs is None else evs
-    
+
     def __str__(self):
         return '#%03d %-10s %s' % (self.id, self.name, self.evs)
 
 
 class Pokemon(object):
-    
+
     @classmethod
     def from_dict(cls, dict):
         import pokedex
@@ -102,25 +102,25 @@ class Pokemon(object):
         self.item = item
         self.pokerus = pokerus
         self.evs = EvSet() if evs is None else evs
-    
+
     name = property(lambda self: self.get_name(),
                     lambda self, name: self.set_name(name))
-    
+
     item = property(lambda self: self._item,
                     lambda self, item: self.set_item(item))
-    
+
     def get_name(self):
         return self.species.name if self._name is None else self._name
-    
+
     def set_name(self, name):
         if name is not None and len(name.strip()) > 0:
             self._name = name.strip()
-    
+
     def set_item(self, item):
         if item is not None and item not in ITEMS:
             raise ValueError("Invalid item '%s'" % item)
-        self._item = ITEMS[item]
-    
+        self._item = ITEMS[item] if item is not None else None
+
     def __str__(self):
         name = self.name
         if self._name is not None:
@@ -142,11 +142,11 @@ class Pokemon(object):
     def listing(self, active):
         padding = '* ' if self is active else '  '
         return '%s%s' % (padding, self)
-    
+
     def battle(species, number=1):
         '''
         Alter's a tracked Pokemons EVs to simulate having battled a Species.
-        These values are altered by pokerus and any item held. The EV 
+        These values are altered by pokerus and any item held. The EV
         increment can be multiplied by number to simulate multiple battles.
         '''
         evs = species.evs.clone()
@@ -155,7 +155,7 @@ class Pokemon(object):
         if self.pokerus:
             evs *= 2
         self.evs += evs * number
-    
+
     def to_dict(self):
         return {'species': self.species.id, 'name': self._name,
                 'pokerus': self.pokerus, 'item': self.item,
